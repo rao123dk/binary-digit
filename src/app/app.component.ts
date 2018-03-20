6026fbbe6046a108141cd06504a346857918dda9
rao123dk <dheerajkrao1993@gmail.com>
2018-03-20 12:32:46 +0530

    Base converter form created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Base converter';
+  title = "Base converter";
+  form;
+  ngOnInit() {
+    this.form = new FormGroup({
+      decimal: new FormControl(""),
+      binary: new FormControl(""),
+      octal: new FormControl(""),
+      hexa: new FormControl("")
+    });
+  }
+
+  decimalChange = function(oldvalue, newValue) {
+    this.form.patchValue({binary : parseInt(newValue, 10).toString(2)});
+  };
 }
